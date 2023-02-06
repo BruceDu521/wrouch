4b9aa4b1e9a820d7165bbe2474063be50ec5ac4f
dubingxuan <dubingxuan@bianfeng.com>
2023-02-06 17:37:55 +0800

    added options defination

--- src/executor.rs
+++ src/executor.rs
@@ -1,10 +1,23 @@
 use wrench::FileManager;
-use clap::{Parser};
 
-#[derive(Parser)]
+use clap::{Parser, ArgAction};
+
+#[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
     file_names: Option<Vec<String>>,
+
+    #[arg(short, long, action=ArgAction::SetTrue, help="change only the access time")]
+    access: Option<String>,
+
+    #[arg(short='c', long="no-create", action=ArgAction::SetTrue, help="do not create any files")]
+    no_create: Option<bool>,
+
+    #[arg(short, long, help="parse STRING and use it instead of current time")]
+    date: Option<String>,
+
+    #[arg(short, long, action=ArgAction::SetTrue, help="change only the modification time")]
+    modification: Option<String>
 }
 
 pub struct Executor {
