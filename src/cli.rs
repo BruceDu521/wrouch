eb597bf8d32d07f772eb34e7013d9c401a16be2c
dubingxuan <dubingxuan@bianfeng.com>
2023-02-09 11:18:31 +0800

    added -r/--reference

--- src/cli.rs
+++ src/cli.rs
@@ -1,12 +1,12 @@
+use std::fs;
 use std::fs::{File, FileTimes};
+use std::io::Result;
 use std::path::Path;
 use std::process;
-use std::io::Result;
 use std::time::SystemTime;
 
-use clap::{Parser, ArgAction};
 use chrono::{DateTime, Local, TimeZone};
-
+use clap::{ArgAction, Parser};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -19,11 +19,18 @@ pub struct Cli {
     #[arg(short='c', long="no-create", action=ArgAction::SetTrue, help="do not create any files")]
     no_create: Option<bool>,
 
-    #[arg(short, long, help="parse STRING and use it instead of current time, e.g: '2001-01-01 12:02:03'")]
+    #[arg(
+        short,
+        long,
+        help = "parse STRING and use it instead of current time, e.g: '2001-01-01 12:02:03'"
+    )]
     date: Option<String>,
 
     #[arg(short, long, action=ArgAction::SetTrue, help="change only the modification time")]
-    modification: Option<bool>
+    modification: Option<bool>,
+
+    #[arg(short, long, help = "use this file's times instead of current time")]
+    reference: Option<String>,
 }
 
 pub struct Executor {
@@ -40,23 +47,19 @@ impl Executor {
             match self.execute_files(file_paths) {
                 Ok(()) => return,
                 Err(err) => {
-                    println!("Wrouch error: {:?}", err);
-                    process::exit(1);
+                    exit(format!("Wrouch error: {:?}", err), 1);
                 }
             }
         } else {
-            println!("No file paths to be found, type 'wrouch --help/-h' to learn how to use wrouch.");
-            process::exit(1);
+            exit(format!(
+                "No file paths to be found, type 'wrouch --help/-h' to learn how to use wrouch."), 
+                1);
         };
     }
 
     fn execute_files(&self, file_paths: &[String]) -> Result<()> {
         let mut file: File;
-        let datetime = self.get_time();
-        let mut mod_time = FileTimes::new();
-        let mut acc_time = FileTimes::new();
-        mod_time = mod_time.set_modified(datetime);
-        acc_time = acc_time.set_accessed(datetime);
+        let (acc_time, mod_time) = self.get_time();
         for path in file_paths {
             let p = Path::new(path);
             if !p.exists() {
@@ -69,31 +72,46 @@ impl Executor {
                 file = File::open(path)?
             }
             if let Err(err) = self.set_times(&file, acc_time, mod_time) {
-                println!("Set time error: {:?}", err);
-                process::exit(1);
+                exit(format!("Set time error: {:?}", err), 1);
             }
         }
         Ok(())
     }
 
-    fn get_time(&self) -> SystemTime {
+    fn get_time(&self) -> (FileTimes, FileTimes) {
+        let mut acc_time = FileTimes::new().set_modified(SystemTime::now());
+        let mut mod_time = FileTimes::new().set_modified(SystemTime::now());
         if let Some(date) = self.cli.date.as_ref() {
             let datetime: DateTime<Local>;
             match Local.datetime_from_str(date.as_str(), "%Y-%m-%d %H:%M:%S") {
                 Ok(dt) => datetime = dt.into(),
-                Err(_) => {
-                    match Local.datetime_from_str(date.as_str(), "%Y-%m-%d") {
-                        Ok(dt) => datetime = dt.into(),
-                        Err(_) => {
-                            println!("Cannot parse datetime from {:?}", date);
-                            process::exit(1);
-                        }
+                Err(_) => match Local.datetime_from_str(date.as_str(), "%Y-%m-%d") {
+                    Ok(dt) => datetime = dt.into(),
+                    Err(_) => {
+                        exit(format!("Cannot parse datetime from {:?}", date), 1);
+                        process::exit(1);
                     }
+                },
+            }
+            acc_time = acc_time.set_accessed(datetime.into());
+            mod_time = mod_time.set_modified(datetime.into());
+        } else if let Some(refer) = self.cli.reference.as_ref() {
+            let meta = fs::metadata(Path::new(refer));
+            match meta {
+                Ok(md) => {
+                    match md.accessed() {
+                        Ok(t) => acc_time = mod_time.set_accessed(t),
+                        Err(err) => exit(format!("Get time of reference file {:?} error: {:?}", refer, err), 1)
+                    }
+                    (md.accessed().unwrap());
+                    mod_time = mod_time.set_modified(md.modified().unwrap());
+                }
+                Err(err) => {
+                    exit(format!("failed to get attributes of '{:?}': {:?}", refer, err), 1);
                 }
             }
-            return datetime.into();
         }
-        SystemTime::now()
+        (acc_time, mod_time)
     }
 
     fn set_times(&self, file: &File, acc_time: FileTimes, mod_time: FileTimes) -> Result<()> {
@@ -107,4 +125,9 @@ impl Executor {
         }
         Ok(())
     }
+}
+
+fn exit(msg: String, code: i32) {
+    println!("{:?}", msg);
+    process::exit(code);
 }
